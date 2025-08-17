5d67395edb74f6e500746e08fe8fdaa342e42ae1
Janis Lesinskis <consulting@lesinskis.com>
2025-08-17 15:52:53 +1000

    Remove seemingly irrelvant source code

--- medium_multiply/__init__.py
+++
@@ -1,2 +0,0 @@
-# This line of code will allow shorter imports
-from medium_multiply.multiplication import Multiplication
--- tests/idw_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class MultiplicationTestCase(unittest.TestCase):
-
-    def test_zero(self):
-        
-        self.assertEqual(1, 1) # dummy test class
