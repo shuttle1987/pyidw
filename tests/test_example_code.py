6e77766c544f0a82056672a6aacce8a725dc9c15
Janis Lesinskis <consulting@lesinskis.com>
2025-08-20 17:26:02 +1000

    Create new test case for IndexError bug

--- tests/test_example_code.py
+++ tests/test_example_code.py
@@ -39,9 +39,12 @@ def test_futurewarning_idw_interpolation(pytestconfig):
     )
 
 def test_index_error_bug(pytestconfig):
-    """This code caused an IndexError to be raised after updating various underlying dependencies.
+    """
+    The implementation of `regression_idw_interpolation` caused an IndexError to be raised
+    after updating various underlying dependencies.
     
-    This test case serves as a regression test for that"""
+    This test case serves as a regression test for that.
+    """
     from pyidw import idw
 
     example_data_directory = pytestconfig.rootdir / "tests" / "example_data"
@@ -60,4 +63,31 @@ def test_index_error_bug(pytestconfig):
         search_radius=5,
         output_resolution=250,
         render_map=False,
+    )
+
+
+def test_index_error_bug_accuracy_regression_idw(pytestconfig):
+    """
+    The implementation of `accuracy_regression_idw` caused an IndexError to be raised
+    after updating various underlying dependencies.
+
+    This test case serves as a regression test for that.
+    """
+    from pyidw import idw
+
+    example_data_directory = pytestconfig.rootdir / "tests" / "example_data"
+
+    bangladesh_temperature_file = example_data_directory / "Bangladesh_Temperature.shp"
+    bangladesh_extent_file = example_data_directory / "Bangladesh_Border.shp"
+    elevation_input_raster_filename = example_data_directory / "Bangladesh_Elevation.tiff"
+
+    original_value, interpolated_value = idw.accuracy_regression_idw(
+        input_point_shapefile=str(bangladesh_temperature_file),
+        input_raster_file=str(elevation_input_raster_filename),
+        extent_shapefile=str(bangladesh_extent_file),
+        column_name="Min_Temp",
+        power=2,
+        polynomial_degree=1,
+        search_radius=5,
+        output_resolution=250,
     )
